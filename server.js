141418b93de850b1d2dfb48396cc56a59f504fb0
Max Corrent <maxcorrent@gmail.com>
2026-03-15 21:36:48 -0400

    Fix Express app initialization

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import ical from "ical";
+import path from "path";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 import axios from "axios";
 
 const fetchMergedCalendar = async () => {
